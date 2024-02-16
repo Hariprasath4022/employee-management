daa320acf7fc429d59f2d97c4b1daf004290c3e4
Hariprasath4022 <hariprasathdoctor2001@gmail.com>
2024-02-16 13:09:36 +0530

    Employee Management Completed

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.security.auth.message.callback.PrivateKeyCallback.Request;
 import jakarta.validation.Valid;
@@ -29,122 +30,154 @@ import net.javaguides.springboot.service.RecordAlreadyExistsException;
 @Controller
 public class EmployeeController {
 
-	private static final Logger log=LoggerFactory.getLogger(EmployeeController.class);
-	
+	private static final Logger log = LoggerFactory.getLogger(EmployeeController.class);
+
 	@InitBinder
 	public void initBlinder(WebDataBinder dataBinder)
 	{
-		StringTrimmerEditor stringTrimmerEditor=new StringTrimmerEditor(true);
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
 		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
 	}
-	
-    @Autowired
-    private EmployeeService employeeService;
 
-    // display list of employees
-    @GetMapping("/")
-    public String viewHomePage(Model model) 
-    {
-        model.addAttribute("listEmployees", employeeService.getAllEmployees());
-        return "index";
-    }
-    
+	@Autowired
+	private EmployeeService employeeService;
 
-    @GetMapping("/showNewEmployeeForm")
-    public String showNewEmployeeForm(Model model)
-    {
-        // create model attribute to bind form data
-        Employee employee = new Employee();
-        model.addAttribute("employee", employee);
-        return "new_employee";
-    }
-    
-    @PostMapping("/saveEmployee")
-    public String saveEmployee(@Valid Employee employee,BindingResult bindingResult,Model model)
-    {
-        // save employee to database
-    		if(bindingResult.hasErrors())
-    		{
-    			return "new_employee";
-    		}
-    		
-    		log.info(">> employee : {}",employee.toString());
+	// display list of employees
+	@GetMapping("/")
+	public String viewHomePage(Model model)
+	{
+		model.addAttribute("listEmployees", employeeService.getAllEmployees());
+		return "index";
+	}
 
-    	try
-        {
-    		employeeService.saveEmployee(employee);
-    		return "redirect:/";
-    	}
-    	catch (RecordAlreadyExistsException e)
-    	{
-            model.addAttribute("error", e.getMessage());
-            return "new_employee"; // Redirect with error message as query parameter
-        }
-    }
-    
-    @PostMapping("/saveUpEmployee")
-    public String saveupEmployee(@ModelAttribute("employee") @Valid Employee employee,BindingResult bindingResult,Model model)
-    {
-        // save employee to database
-    		if(bindingResult.hasErrors())
-    		{
-    			return "update_employee";
-    		}
-    		
-    		log.info(">> employee : {}",employee.toString());
-    		
+	@GetMapping("/showNewEmployeeForm")
+	public String showNewEmployeeForm(Model model)
+	{
+		// create model attribute to bind form data
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+		return "new_employee";
+	}
+
+	@PostMapping("/saveEmployee")
+	public String saveEmployee(@Valid Employee employee, BindingResult bindingResult, Model model)
+	{
+		// save employee to database
+		if (bindingResult.hasErrors()) {
+			return "new_employee";
+		}
+
+		log.info(">> employee : {}", employee.toString());
+
+		try {
+			employeeService.saveEmployee(employee);
+			return "redirect:/";
+		} catch (RecordAlreadyExistsException e) {
+			model.addAttribute("error", e.getMessage());
+			return "new_employee"; // Redirect with error message as query parameter
+		}
+	}
+
+	@GetMapping("/saveEmployee")
+	public String saveEmployeeR(@Valid Employee employee, BindingResult bindingResult, Model model) {
+		// save employee to database
+		if (bindingResult.hasErrors()) {
+			return "new_employee";
+		}
+
+		log.info(">> employee : {}", employee.toString());
+
+		try {
+			employeeService.saveEmployee(employee);
+			return "redirect:/";
+		} catch (RecordAlreadyExistsException e) {
+			model.addAttribute("error", e.getMessage());
+			return "new_employee"; // Redirect with error message as query parameter
+		}
+	}
+
+	@PostMapping("/saveUpEmployee/{id}")
+	public String saveupEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult,
+			Model model)
+	{
+		// save employee to database
+		if (bindingResult.hasErrors()) {
+			return "update_employee";
+		}
+
+		log.info(">> employee : {}", employee.toString());
+
 //		 if (!employeeService.isAnyFieldUpdated(employee))
 //		 {
 //	        return "redirect:/"; // No fields were updated, redirect to the employee list page
 //	     }
-		
-			 try
-		        {
-		    		employeeService.saveupEmployee(employee);
-		    		return "redirect:/";
-		    	}
-		    	catch (RecordAlreadyExistsException e)
-		    	{
-		            model.addAttribute("error", e.getMessage());
-		            return "update_employee"; // Redirect with error message as query parameter
-		        }
-		 
-    }
 
-    @GetMapping("/showFormForUpdate/{id}")
-    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) 
-    {
+		try {
+			employeeService.saveupEmployee(employee);
+			return "redirect:/";
+		} catch (RecordAlreadyExistsException e) {
+			model.addAttribute("error", e.getMessage());
+			return "update_employee"; // Redirect with error message as query parameter
+		}
 
-        // get employee from the service
-        Employee employee = employeeService.getEmployeeById(id);
+	}
 
-        // set employee as a model attribute to pre-populate the form
-        model.addAttribute("employee", employee);
-        return "update_employee";
-    }
-    
-    //@GetMapping("/deleteconfirm/{id}")
-    @RequestMapping(value = "/deleteconfirm/{id}", method = RequestMethod.GET)
-    public String showDeleteConfirmationModal(@PathVariable Long id, Model model) {
-        model.addAttribute("employee", employeeService.getEmployeeById(id));
-        return "deleteconfirmation";
-    }
+	@GetMapping("/saveUpEmployee/{id}")
+	public String showFormForUpdateR(@PathVariable(value = "id") long id, Model model)
+	{
 
-    //@PostMapping("/delete/__${id}__")
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
-    public String deleteEmployee(@PathVariable Long id) {
-        this.employeeService.deleteEmployeeById(id);
-        return "redirect:/";
-    }
-    
-    
-//    @PostMapping("/deleteEmployee/{id}")
-//    public String deleteEmployee(@PathVariable(value = "id") long id)
-//    {
-//
-//        // call delete employee method 
-//    	
-//        this.employeeService.deleteEmployeeById(id);
-//        return "redirect:/";
-//    }
+		// get employee from the service
+		Employee employee = employeeService.getEmployeeById(id);
+
+		// set employee as a model attribute to pre-populate the form
+		model.addAttribute("employee", employee);
+		return "update_employee";
+	}
+
+	@GetMapping("/showFormForUpdate/{id}")
+	public String showFormForUpdate(@PathVariable(value = "id") long id, Model model)
+	{
+
+		// get employee from the service
+		Employee employee = employeeService.getEmployeeById(id);
+
+		// set employee as a model attribute to pre-populate the form
+		model.addAttribute("employee", employee);
+		return "update_employee";
+	}
+
+	
+	 
+	  
+	  //@RequestMapping(value = "/deleteconfirm/{id}", method = RequestMethod.POST)
+	@GetMapping("/deleteconfirm/{id}")
+	public String showDeleteConfirmationModal(@PathVariable Long id, Model model)
+	{
+		model.addAttribute("employee", employeeService.getEmployeeById(id)); 
+		return "deleteconfirmation"; 
+	}
+	 
+
+	 //@PostMapping("/delete/__${id}__")
+	  //@RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+	@PostMapping("/delete/{id}")
+	public String deleteEmployee(@PathVariable Long id,RedirectAttributes redirectAttributes, Model model)
+	{
+	  this.employeeService.deleteEmployeeById(id); 
+	  redirectAttributes.addFlashAttribute("deleteSuccess", true);
+	  return "redirect:/";
+	}
+	 
+
+	/*
+	 * @RequestMapping(value = "/deleteEmployee/{id}", method = RequestMethod.GET)
+	 * public String deleteEmployee(@PathVariable(value = "id") long id,
+	 * RedirectAttributes redirectAttributes, Model model) {
+	 * 
+	 * // call delete employee method
+	 * 
+	 * this.employeeService.deleteEmployeeById(id);
+	 * redirectAttributes.addFlashAttribute("deleteSuccess", true); return
+	 * "redirect:/"; }
+	 */
 }
